250908a53ad5f2c02148fe0f34897687bb8e934e
Luke <lukerebeiro@hotmail.co.uk>
2020-10-23 11:09:07 +0100

    refactor to conroller

--- freshproject/instaclone/resources/views/profiles/index.blade.php
+++ freshproject/instaclone/resources/views/profiles/index.blade.php
@@ -34,9 +34,9 @@
                 @endcan
 
                 <div class="d-flex">
-                    <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> Posts</div>
-                <div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> Followers</div>
-                <div class="pr-5"><strong>{{ $user->following->count() }}</strong> Following</div>
+                    <div class="pr-5"><strong>{{ $postCount }}</strong> Posts</div>
+                <div class="pr-5"><strong>{{ $followersCount }}</strong> Followers</div>
+                <div class="pr-5"><strong>{{ $followingCount }}</strong> Following</div>
                 </div>
                 <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
                 <div>{{ $user->profile->description }}</div>
