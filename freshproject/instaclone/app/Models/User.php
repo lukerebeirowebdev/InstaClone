26d51476dbc371398f7740059ea1d462f32a710a
Luke <lukerebeiro@hotmail.co.uk>
2020-10-23 12:40:16 +0100

    commit

--- freshproject/instaclone/app/Models/User.php
+++ freshproject/instaclone/app/Models/User.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
 use PhpParser\Builder\Function_;
 
 class User extends Authenticatable
@@ -43,17 +45,20 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    // protected static function boot()
-    //     {
-    //         parent::boot();
+     protected static function boot()
+         {
+            parent::boot();
 
-    //         static::created(function ($user) {
-    //             $user->profile()->create([
-    //                 'title' => $user->username,
-    //             ]);
-    //         });
+             static::created(function ($user) {
+                $user->profile()->create([
+                     'title' => $user->username,
+                ]);
 
-    //     }
+            Mail::to($user->email)->send(new NewUserWelcomeMail());
+            });
+
+        
+         }
 
     public function getProfileImageAttribute()
     {
