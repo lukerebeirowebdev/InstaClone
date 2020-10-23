9abf0a9c96a53059534cca5915c26cd4df2f93da
Luke <lukerebeiro@hotmail.co.uk>
2020-10-23 11:09:52 +0100

    commit

--- freshproject/instaclone/resources/views/posts/index.blade.php
+++ freshproject/instaclone/resources/views/posts/index.blade.php
@@ -5,14 +5,14 @@
         @foreach ($posts as $post)
 
             <div class="row">
-                <div class="col8 offset-2">
+                <div class="col-6 offset-3">
                     <a href="/profile/{{ $post->user->id }}">
                         <img src="/storage/{{ $post->image }}" class="w-100"></a>
                 </div>
             </div>
 
             <div class="row pt-2 pb-4">
-                <div class="col8 offset-2">
+                <div class="col-6 offset-3">
                     <p>
                         <span class="font-weight-bold">
                             <a href="/profile/{{ $post->user->id }}">
@@ -23,5 +23,11 @@
                 </div>
             </div>
         @endforeach
+
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                {{ $posts->links() }}
+            </div>
+        </div>
     </div>
 @endsection
--- freshproject/instaclone/resources/views/posts/show.blade.php
+++ freshproject/instaclone/resources/views/posts/show.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col8">
+            <div class="col-8">
                 <img src="/storage/{{ $post->image }}" class="w-100">
             </div>
 
-            <div class="col4">
+            <div class="col-4">
                 <div>
                     <div class="d-flex align-items-center">
                         <div class="pr-3">
