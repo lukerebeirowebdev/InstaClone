c60b106de12167d0bed013779603fe730ee0ebf5
Luke <lukerebeiro@hotmail.co.uk>
2020-10-23 12:39:41 +0100

    added mailing

---
+++ freshproject/instaclone/resources/views/email/welcome-email.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# Welcome to the new InstaGram! 
+
+This is a community of fellow developers and we love that you have joined us.
+
+All the best,
+
+Luke
+@endcomponent
